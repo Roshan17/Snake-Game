f025f8005bae2909592aef51c60343c74f031c05
Roshan Nadeesha <roshan@debian>
2020-08-19 12:09:18 +0530

    projection mode with reshape function added

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include <GL/glut.h>
 
 void display_callback();
+void reshape_callback();
+
+void init(){
+    glClearColor(1.0,1.0,1.0,1.0);
+}
 
 int main(int argc, char **argv){
     glutInit(&argc,argv);
@@ -9,11 +14,22 @@ int main(int argc, char **argv){
     glutInitWindowSize(500,500);
     glutCreateWindow("SNAKE GAME");
     glutDisplayFunc(display_callback);
+    glutReshapeFunc(reshape_callback);
+    init();
     glutMainLoop();
     return 0;
 
 }
 
 void display_callback(){
+    glClear(GL_COLOR_BUFFER_BIT);
     glutSwapBuffers();
+}
+
+void reshape_callback(int w, int h){
+    glViewport(0.0,0.0,(GLsizei)w,(GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0,40.0,0.0,40.0,-1.0,1.0);
+    glMatrixMode(GL_MODELVIEW);
 }
