97fee1ae683268f8829b290080b9df15125405e1
Roshan Nadeesha <roshan@debian>
2020-08-19 10:37:47 +0530

    Initial Commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+void display_callback();
+
+int main(int argc, char **argv){
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("SNAKE GAME");
+    glutDisplayFunc(display_callback);
+    glutMainLoop();
+    return 0;
+
+}
+
+void display_callback(){
+    glutSwapBuffers();
+}
